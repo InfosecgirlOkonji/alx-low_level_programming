c34298e74bfcaf963c3b7d93216099dda7bcabb7
InfosecgirlOkonji <okonjipeninah15@gmail.com>
2023-03-20 13:04:31 -0700

    Comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
- *
- * Return: 0
+ * Description: prints numbers
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = '0'; i <= '9' ; i++)
+	{
 		putchar(i);
-	for (i = 'a'; i <= 'f'; i++)
-		putchar(i);
+
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
 	putchar('\n');
 	return (0);
 }
